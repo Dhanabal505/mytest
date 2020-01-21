ea8c0f099d45a5fa985cdc1c80683c76d4f6acdc
Dhanabal505 <psdhanabal505>
2020-01-21 04:36:01 -0800

    Firsttest

--- mytest/ViewController.swift
+++ mytest/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var x:String = "Dhanabal"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
